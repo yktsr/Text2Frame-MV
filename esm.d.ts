f2e445e7f9683537c6805faf89223ab945879efd
Yuki Katsura <electric.sheep.box@gmail.com>
2024-01-09 00:32:13 +0900

    viteの成果物を追加

--- esm.d.ts
+++ esm.d.ts
@@ -1,4 +1,4 @@
-declare module "Text2Frame-MV/Text2Frame.mjs" {
+namespace Text2FrameMV {
   /**
    * Text2Frameの文法で書かれた文字列をRPG Maker MV/MZのイベントコマンドリストに変換します。
    * 戻り値にはMapの定義は含まれず、イベントコマンドリストのみが返るため、Mapへの組み込みは各自で行なってください。
@@ -6,7 +6,20 @@ declare module "Text2Frame-MV/Text2Frame.mjs" {
    * The return value only includes the event command list and does not contain the Map definition. Therefore, integration into the Map should be done individually.
    * @param text Text2Frameの文法に従って書かれた文字列
    */
-  export function convert(
+  export function compile(
     text: string
   ): { code: number; parameters: any[]; indent: number }[];
+}
+
+declare module "Text2Frame-MV" {
+  export = Text2FrameMV;
+}
+declare module "Text2Frame-MV/Text2Frame.cjs.js" {
+  export = Text2FrameMV;
+}
+declare module "Text2Frame-MV/Text2Frame.es.js" {
+  export = Text2FrameMV;
+}
+declare module "Text2Frame-MV/Text2Frame.umd.js" {
+  export = Text2FrameMV;
 }
