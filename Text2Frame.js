843c2600afe1a2b96b8825217cb2ba1c8b26867e
Yuki Katsura <electric.sheep.box@gmail.com>
2024-01-30 00:05:35 +0900

    fix object-shorthand

--- Text2Frame.js
+++ Text2Frame.js
@@ -4121,7 +4121,7 @@
         BASE_PATH = path.dirname(process.mainModule.filename)
       }
 
-      return { PATH_SEP: PATH_SEP, BASE_PATH: BASE_PATH }
+      return { PATH_SEP, BASE_PATH }
     }
 
     const getDefaultPage = function () {
